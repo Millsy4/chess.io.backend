83a1a49da84ed3c48540c54672ec4a8e3fd74de2
Christian Mills <millsy4@hotmail.com>
2022-01-06 15:40:35 -0600

    added get users path

--- src/main/java/com/projecttwo/chess/io/controller/UserController.java
+++ src/main/java/com/projecttwo/chess/io/controller/UserController.java
@@ -28,22 +28,26 @@ public class UserController {
 	@Autowired
 	UserRepository userRepository;
 
-//	@GetMapping("/users")
-//	public ResponseEntity<List<User>> getAllUsers() {
-//		try {
-//			List<User> users = new ArrayList<User>();
-//
-//			userRepository.findAll().forEach(users::add);
-//			
-//			if (users.isEmpty()) {
-//				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//			}
-//
-//			return new ResponseEntity<>(users, HttpStatus.OK);
-//		} catch (Exception e) {
-//			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//	}
+	@GetMapping("/users")
+	public ResponseEntity<List<User>> getAllUsers() {
+		try {
+			List<User> users = new ArrayList<User>();
+			
+			// username
+			// name
+			// profile picutre
+			
+			userRepository.findAll().forEach(users::add);
+			
+			if (users.isEmpty()) {
+				return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+			}
+
+			return new ResponseEntity<>(users, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
 	@GetMapping("/users/{username}")
 	public ResponseEntity<User> getUserById(@PathVariable("username") String username) {
