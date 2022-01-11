bfc4cd95b078b8e7895608f6763d94c0abce0ee0
Christian Mills <millsy4@hotmail.com>
2022-01-11 01:29:04 -0600

    small changes

--- src/main/java/com/projecttwo/chess/io/repository/UserRepository.java
+++ src/main/java/com/projecttwo/chess/io/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.projecttwo.chess.io.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projecttwo.chess.io.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 	
 }
